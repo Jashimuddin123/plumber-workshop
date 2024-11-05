0c4b41d06772ee6afb1a03b673c3adcf16d8666c
Jashimuddin123 <adnanmahmud3394@gmail.com>
2024-11-05 23:47:11 +0600

    added a feature for single pages

--- src/Components/LinkableFiles/Dordrecht.jsx
+++ src/Components/LinkableFiles/Dordrecht.jsx
@@ -13,51 +13,53 @@ const Dordrecht = () => {
         <div className="flex flex-col md:flex-row shadow-2xl ml-12 px-6">
             {/* Content div on the left */}
             <div className="flex-1 w-full md:w-2/3 pr-4">
-            <h1 className="tex-4xl text-5xl font-bold">Sewerage</h1>
-            <div className="border-4 border-sky-400 w-28 my-6"></div>
-                <img src="https://i.ibb.co/SRfvZXn/24uurloodgieter-baner-slide1.jpg" alt="" />
-                <button className="btn my-4 btn-info text-white rounded-full text-xl py-2 px-4">CALL URGENTLY: 085_0026311</button>
-                <h1 className="text-3xl md:text-5xl font-bold my-4 text-left">Sewer problems?</h1>
-                <p className="text-lg md:text-xl my-4 text-gray-500 text-left">Is your sewer blocked? Is the water no longer draining? Contact our 24-hour Plumber service. We are available day and night for all conceivable sewer problems.</p>
-                <img src="https://i.ibb.co/bsvFvkt/waterpipe-lig.jpg" alt="" />
+            <h1 className="text-5xl font-bold">Loodgieter Alphen aan den Rijn</h1>
+            <div className="border-4 border-sky-400 w-28 my-10"></div>
+               <p className="text-gray-500 text-left text-xl space-y-8 my-4">Plumber Alphen aan den Rijn : Are you looking for a plumber in Alphen aan den Rijn ? Then contact us directly on 085 – 0026311  and get in touch with the plumber in Alphen aan den Rijn . We are available 24 hours a day, 7 days a week for all conceivable inconveniences. Leakage ? Blockage? Problems with your central heating? Our 24 hour plumbers are always available for you.</p>
+             
+             <h1 className=" text-3xl font-bold text-sky-500 mt-10">24 hour Plumber in Alphen aan den Rijn</h1>
+             <p className="text-gray-500 text-left text-xl space-y-4 my-4">The 24 hour Plumber in Alphen aan den Rijn is available for you day and night. We are available for all conceivable emergencies such as leaks, blockages or gas problems. You can reach us 24 hours a day, 7 days a week for all conceivable emergencies on telephone number 085 - 0026311 .
 
-                <div>
+             In addition to our 24-hour plumber service, we also have our own cv fitters. Do you need a certified cv fitter immediately because the cv is no longer getting warm, you no longer have hot water or because you have a cv boiler malfunction? Then call our service number.</p>
+             <div>
                     <h1 className="text-3xl md:text-3xl font-bold text-sky-400 text-left">Sewerage works</h1>
-                    <h1 className="text-gray-600 text-left text-lg md:text-xl">You can contact us 24/7 for:</h1>
+                    <h1 className="text-gray-500 font-bold text-left text-2xl my-6 md:text-2xl">
+                    You can reach us 24/7 for, among other things::</h1>
                     <ul className="ml-6 text-lg md:text-xl mb-8 space-y-4 text-gray-500 list-disc mt-4">
-                        <li>Unblocking drains of toilets, showers, and sinks</li>
-                        <li>Problems with draining rainwater</li>
-                        <li>Pumping out crawl spaces, cellars, and basements</li>
-                        <li>How to Fix Bubbling Drains</li>
-                        <li>Remedying sewer odor “sewer smell”</li>
-                        <li>Leakage due to leaking gutters or downpipes</li>
-                        <li>Various forms of odor nuisance from the sewer</li>
-                        <li>Construction of a completely new sewerage system</li>
+                        <li>Toilet Blockage</li>
+                        <li>Drain Blockage</li>
+                        <li>Sewer Problems</li>
+                        <li>Leakage</li>
+                        <li>Problems with central heating boiler</li>
+                        <li>CV installation problems</li>
+                        <li>Flooded basements and crawl spaces</li>
+                      
                     </ul>
                 </div>
 
                 <div className="container flex flex-col md:flex-row">
                     <div className="text-content w-full md:w-1/2">
-                        <h1 className="text-3xl font-bold text-sky-400 text-left my-4">Sewer clogged? Call the Plumber</h1>
-                        <p className="text-gray-500 text-left text-lg md:text-xl my-4">A common problem is a clogged toilet, often caused by excessive use of toilet paper...</p>
-                        <p className="text-gray-500 text-left text-lg md:text-xl my-4">In addition, various liquid or non-liquid drain unblockers can cause major damage to the sewer or drain...</p>
+                        <h1 className="text-3xl font-bold text-sky-400 text-left my-4">
+                        Blockages</h1>
+                       <p className="text-gray-500 text-left text-xl space-y-4 my-4">Perhaps there is nothing more annoying than a blockage. Is your toilet blocked? Or do you have a blockage in the sewer ? Then contact our plumbers in Alphen aan den Rijn . We have the most modern equipment and tools to detect and resolve all types and sizes of blockages.
+
+                      We unclog both drain pipes and complete sewer systems. Need a professional who can unclog your drain pipes right away? Then contact our 24-hour plumber Alphen aan den Rijn service.</p>
                     </div>
                     <div className="image-content w-full md:w-1/2">
-                        <img className="h-60 md:h-96 mt-10 w-full" src="https://i.ibb.co/tYPYh1r/24-loodgieter-181x300.jpg" alt="Clogged Sewer" />
+                        <img className="h-60 md:h-96 mt-10 w-full"
+                        src="https://i.ibb.co/mJCmMJR/loodgieter-alphen-aan-den-rijn-300x235.jpg" alt="Clogged Sewer" />
                     </div>
-                </div>
 
-                <div>
-                    <h1 className="text-3xl font-bold text-sky-400 text-left">Drain clogged</h1>
-                    <p className="text-left text-lg md:text-xl my-4 text-gray-500">A clogged drain in the kitchen or bathroom? Then contact us on 085 – 0026311...</p>
-                    <h1 className="text-3xl my-4 font-bold text-sky-400 text-left">Unblocking the sewer by the plumber</h1>
-                    <p className="text-left text-lg md:text-xl my-4 text-gray-500">How do I unclog the sewer? That is a question we hear a lot...</p>
-                    <h1 className="text-3xl font-bold my-4 text-sky-400 text-left">Plumber 24 hours sink clogged</h1>
-                    <p className="text-left text-lg md:text-xl my-4 text-gray-500">Is the sink clogged or is the water no longer draining? Call our emergency number...</p>
-                    <img className="w-full h-60 md:h-[500px]" src="https://i.ibb.co/bsvFvkt/waterpipe-lig.jpg" alt="" />
+                   
                 </div>
+                    <h1 className="text-3xl font-bold text-sky-500 my-6">Direct assistance in case of leakage</h1>
+                    <p className="text-gray-500 text-left text-xl space-y-8 my-4">Leakage in kitchen or bathroom? This can have various causes, for example there can be a blockage that pushes the water up and in this way causes a leak. In addition, a common cause of leakage is a burst or ruptured water pipe. In some cases this is difficult to reach because pipes are generally concealed in walls, ceilings and floors.
+
+                   Our plumbers in Alphen aan den Rijn have the knowledge and experience to be  able to find out what causes such a leak. In addition, we have various devices, tools and gadgets to detect a leak and tackle it at the cause.
+
+                   Do you want to speak to a plumber in Alphen aan den Rijn directly to make an appointment? Then contact us via the service number 085 - 0026311</p>
+              
             </div>
-
             {/* Links div on the right */}
             <div className="flex-1 w-full md:w-1/3 pl-4 mt-10">
                     <h1 className="text-3xl font-bold text-sky-400 my-4">NEED A PLUMBER URGENTLY?</h1>
